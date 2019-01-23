c5fd97cd634599d539fb52a7f644980b3044c647
root <1075118340@qq.com>
2019-01-23 13:47:31 +0800

    lalla

---
+++ src/main/java/com/jk/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.jk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping("toView")
+    public String toView(String viewName){
+
+        return viewName;
+    }
+}
