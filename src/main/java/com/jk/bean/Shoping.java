ae63eb81f0d4522822e51430803b3166440abeae
xjw163com <1632622685@qq.com>
2019-01-24 21:01:01 +0800

    属性完成

--- src/main/java/com/jk/bean/Shoping.java
+++ src/main/java/com/jk/bean/Shoping.java
@@ -8,14 +8,12 @@ import java.io.Serializable;
 public class Shoping implements Serializable {
 
     Integer Id;//(编号)          PKInteger
-
     Integer tjshl;//(添加数量)   Integer
-    Integer hj;//(合计)          decimal
     Integer yh_id;//(用户id)     Integer
-    Integer shp_id;//(商品id)    Integer
-
     Integer sku_id;//(skuid)     Integer
-
-    Integer shfxz;//(是否选中)   String(1)
+    Integer price;//(物品单价)
+    Integer hj;//(合计)          decimal//
+    Integer shp_id;//(商品id)    Integer//
+    Integer shfxz;//(是否选中)   String(1)//
 
 }
