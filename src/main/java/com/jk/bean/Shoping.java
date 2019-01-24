f2d4d84764f8dc289ec7d2d8375b0ba44c14353a
windFlde <wind13934954475@163.com>
2019-01-24 23:22:46 +0800

    购物车

--- src/main/java/com/jk/bean/Shoping.java
+++ src/main/java/com/jk/bean/Shoping.java
@@ -9,13 +9,15 @@ public class Shoping implements Serializable {
 
     Integer Id;//(编号)          PKInteger
 
-    Integer tjshl;//(添加数量)   Integer
+    Integer tjshl=1;//(添加数量)   Integer
     Integer hj;//(合计)          decimal
-    Integer yh_id;//(用户id)     Integer
-    Integer shp_id;//(商品id)    Integer
+    Integer yh_id=1;//(用户id)     Integer
+    Integer shp_id=1;//(商品id)    Integer
+    Integer price=200;
+    Integer sku_id=2;//(skuid)     Integer
 
-    Integer sku_id;//(skuid)     Integer
+    Integer shfxz=1;//(是否选中)   String(1)
 
-    Integer shfxz;//(是否选中)   String(1)
+
 
 }
